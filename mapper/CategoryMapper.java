7aa20bb054d776dea72dc93086d52823d340b131
pjiu <1712869177@qq.com>
2021-01-29 16:03:04 +0800

    Add files via upload

---
+++ mapper/CategoryMapper.java
@@ -0,0 +1,35 @@
+package com.testblog.mapper;
+
+import com.testblog.entity.Categories;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+@Mapper
+@Repository
+public interface CategoryMapper {
+
+    @Insert("insert into categories(categoryName) value(#{categoryName})")
+    void save(Categories categories);
+
+    @Select("select categoryName from categories")
+    List<String> findCategoriesName();
+
+    @Select("select count(*) from categories")
+    int countCategoriesNum();
+
+    @Select("select id,categoryName from categories")
+    List<Categories> findAllCategories();
+
+    @Delete("delete from categories where categoryName=#{categoryName}")
+    void deleteCategory(String categoryName);
+
+    @Select("select IFNULL((select id from categories where categoryName=#{categoryName}),0)")
+    int findIsExistByCategoryName(String categoryName);
+
+}
